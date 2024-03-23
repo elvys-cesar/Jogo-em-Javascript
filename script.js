6270293d47ec47b49394e6efe4886cdc76b3d0be
Elvys cesar <biggeniopocket@gmail.com>
2024-03-23 12:40:55 -0300

    correção de bugs e erros. Versão 0.0.2

--- script.js
+++ script.js
@@ -18,6 +18,9 @@ let maoDois = document.getElementById("mao-2")
 let maoTres = document.getElementById("switch")
 
 let computador = document.getElementById("computador")
+let load = document.querySelector(".load");
+
+
 let online = false
 
 var novo = -220;
@@ -26,6 +29,7 @@ let millisecond = 0
 
 let openCamera = document.querySelector(".camera")
 let memoria = document.querySelector(".memoria")
+let del = document.getElementById("delete");
 
 let rom = [
   `background-image: url(scr/scenes/BG-2.png);`,
@@ -46,7 +50,7 @@ let shots = [
 let x = 0;
 wall.style = rom[x];
 
-let lantenaAtiva = false;
+let lanternaAtiva = false;
 
 ///efeitos sonoros///////////////
 let switchlanter = "/scr/audioEfect/light-switch-81967.mp3"
@@ -57,10 +61,10 @@ let computerNoises = "/scr/audioEfect/computer-hum-noise.mp3"
 var cordenadas = maoTres.getBoundingClientRect();
 
 function lanterna() {
-  lantenaAtiva = !lantenaAtiva;
+  lanternaAtiva = !lanternaAtiva;
   playsound(switchlanter);
 
-  if(lantenaAtiva == true){
+  if(lanternaAtiva == true){
     maoUm.style=`display:flex;`
 
   }else{
@@ -74,9 +78,10 @@ maoDois.addEventListener("click", (event) => {
 
   console.log(x, y)
 
-  if (lantenaAtiva == true) {
+
+  if (lanternaAtiva == true) {
     frente.style.webkitMaskImage = `radial-gradient(circle at ${x}px ${y}px, transparent 2px, black 400px)`;
-  } else if (lantenaAtiva == false) {
+  } else if (lanternaAtiva == false) {
     frente.style.webkitMaskImage = `radial-gradient(circle at ${x}px ${y}px, transparent 0px, black 0px)`;
   }
 });
@@ -108,7 +113,7 @@ function timerP(){
     if ((millisecond += 10) == 10) {
         millisecond = 0;
         if (novo > -600){
-        novo-= 3;
+        novo-= 2;
         }
         console.log(novo)
         screen.style = `right: ${novo}px;`;
@@ -117,15 +122,16 @@ function timerP(){
 }
 
 function timerN(){
-    if ((millisecond += 10) > 10) {
+    if ((millisecond += 10) == 10) {
         millisecond = 0;
         if(novo <  320){
-        novo += 3;
+        novo += 2;
         }
         console.log(novo)
         screen.style = `right: ${novo}px;`;
       }
 }
+let porcentagem = 100
 
 function lista() {
   if (x < 3) {
@@ -156,21 +162,20 @@ function lista2() {
 //////////////////
 
 botao_costa.addEventListener("click", function () {
-  fade.style = "animation: fade 2s forwards;"
+  fade.style = "display: flex;"
+  setTimeout(function(){
+    fade.style = "display: none;"
+  },1000)
   setTimeout(lista, 500);
-
-  setTimeout(
-    fundo.style = "filter: ;"
-  );
 });
 
 botao_frente.addEventListener("click", function () {
-  setTimeout(lista2, 1000);
+  fade.style = "display: flex;"
+  setTimeout(function(){
+    fade.style = "display: none;"
+  },1000)
+  setTimeout(lista2, 500);
 
-  setTimeout(
-    document.querySelector(".fadeIN")?.classList.remove("fadeIN"),
-    1500
-  );
 });
 
 setInterval(function () {
@@ -183,12 +188,15 @@ setInterval(function () {
     botao_costa.style = "display: flex;";
   }
 
+
+
+  let on = false
+
+
 //INTERAÇÃO COM O COMPUTADOR///////
   if (x == 1){
-    ComputadorAtivavel()
     portaSala()
-    curtina.style=`display: flex;`
-
+    maoUm
   }else if (x == 2){
     maoUm.style=`
       height: 30px;
@@ -199,7 +207,6 @@ setInterval(function () {
       bottom: 380px;
       opacity: .6;
       `
-  } else if (x == 3){
   }
 
   if (x ==  0){
@@ -207,8 +214,39 @@ setInterval(function () {
   } else {
     curtina.style = 'display: none;'
   }
+  del.addEventListener('mousedown',function(){
+    sobrecarregado(true)
+  })
 
+  del.addEventListener('mouseup',function(){
+    sobrecarregado(false)
+  })
+  if(porcentagem > 0 && on == false && onEnemy == true){
+    console.log("hehehehhesh",onEnemy)
+    porcentagem -= 0.6;
+  } else if (onEnemy == true  && porcentagem > 1){
+    porcentagem = porcentagem
+  } else if (porcentagem <= 1 && onEnemy == true){
+    setTimeout(function(){
+      telefonema(false)
+    }, 2000)
+  }
+  memoriaServe(porcentagem)
 }, 100);
+
+
+
+function sobrecarregado(off){
+  console.log("aparece porra", off)
+  on = off
+  if(porcentagem < 100 && on == true) {
+    porcentagem += 0.05
+  }
+}
+
+function memoriaServe(){
+  load.style = `width: ${porcentagem}%;`
+}
 
 function ComputadorAtivavel() {
   maoUm.style = `
@@ -223,24 +261,26 @@ function ComputadorAtivavel() {
 }
 
 function portaSala(){
-  maoDois.style = `
+  maoUm.style = `
       height: 30px;
       width: 30px;
       position: absolute;
       z-index: 40;
-      right: 300px;
+      left: 300px;
       bottom: 380px;
       opacity: .6;
       `
 }
 
 maoUm.addEventListener("click", function(){
-  teste()
+  if (x == 0){
+  mostrarComputador()
+  }
 })
 
 
 
-function teste(){
+function mostrarComputador(){
   computador.style = `display: flex;`
 }
 
@@ -279,41 +319,85 @@ const classes = stranger.classList;
 
 
 let fundoMusical = document.querySelector(".ambient")
-fundoMusical.volume = .05;
+fundoMusical.volume = .4;
 fundoMusical.loop = true;
 
 
-
-let indoorsteps = "/scr/audioEfect/indoor-footsteps-6385.mp3"
+let indoorsteps = document.querySelector(".steps")
 let heartBt = document.querySelector(".heathB")
 let LostAre = document.querySelector(".lostAre")
 let homem = document.querySelector(".homen")
 let susto = document.querySelector(".medo")
+let telefone = document.getElementById("telefone")
+
+let creak = document.querySelector(".woodCreak")
+let doorKnocks = document.querySelector(".doorKnocks")
+let jumpscary = document.querySelector(".jumps")
+let eyes = document.querySelector(".eyes")
+let mensagemDispensado = document.querySelector(".despedido")
+let telaVitoria = document.querySelector(".win")
+
+
+
+let sucess = document.querySelector(".sucesso")
 
 let tempo = document.getElementById("hora")
 
-const local = [1,2,3,4];
-let onEnemy = true;
 
-let misecond = 0
-let segundo = 0
-let hora =   0
+let randomfoto
+const local = [1,2,3,4,5];
+let onEnemy = false;
 
-
+let hr = 0
 //SE O INIMIGO ESTIVER ATIVO ESSA FUNÇÂO TOCA.
+setTimeout(function(){
+  onEnemy = true
+}, 3000)
+
+
+setInterval(function(){
+  if(hr < 7 && onEnemy == true){
+    hr++
+  }
+  if (porcentagem > 2){
+    moreT(hr)
+  }
+}, 10000)
+
+
+function moreT(a){
+  pm = a
+  tempo.innerHTML=`0${pm}:00`;
+  if (pm == 6){
+    telefonema(true)
+    telefone.pause()
+    sucess.play()
+
+    setTimeout(function(){
+      onEnemy= false
+      return onEnemy
+    }, 2000)
+  }
+}
+
 setInterval(  function(){
-  let randomfoto = Math.floor(Math.random() * local.length);
+
+  if(onEnemy == true){
+    randomfoto = Math.floor(Math.random() * local.length);
+  }
 
   record.style = `${shots[randomfoto]}`
   console.log("foto escolhida"+randomfoto)
 
 
-  if (randomfoto == 2 && onEnemy){
-
-
-    som(indoorsteps)
+  if (randomfoto == 2){
+    doorKnocks.pause()
     LostAre.pause()
-    behindWindow()
+    som()
+
+    setTimeout( function(){
+      behindWindow(randomfoto)
+    }, 5000)
     
     setTimeout( function(){
       heartBt.currentTime = 0
@@ -326,39 +410,81 @@ setInterval(  function(){
     OfbehindWindow()
     heartBt.pause()
     LostAre.pause()
+    doorKnocks.pause()
   } else if (randomfoto == 3) {
     LostAre.currentTime = 0
     OfbehindWindow()
     heartBt.pause()
     LostAre.play()
+    doorKnocks.pause()
+  } else if (randomfoto == 4){
+    heartBt.pause()
+    LostAre.pause()
+    OfbehindWindow()
+    creak.volume = 1.0
+    creak.play()
+    batidaPorta()
   }
 
 
 
 }, 10000)
 
+function batidaPorta (){
+  doorKnocks.currentTime = 0
+  setTimeout(function (){
+    creak.pause()
+    doorKnocks.volume = 0.2
+    doorKnocks.play()
+  }, 3000)
+}
 
-function som(audioName){
-  let ed = new Audio(audioName);
-  ed.play()
-  ed.muted=false;
-  ed.volume = 0.6
+function som(){
+  indoorsteps.play()
 }
 
 
-function behindWindow(){
-  console.log(lantenaAtiva)
-  if(lantenaAtiva == true){
-    susto.play()
-    setTimeout(function(){
-      homem.play()
+function behindWindow(randomfoto){
 
-    },2000)
-    }
+    if(randomfoto == 2 && lanternaAtiva == true){
+      setTimeout(function(){
+        jumpscary.style = "display: flex;"
+        setTimeout(function(){
+          eyes.style = "display: flex;"
+          susto.play()
+        }, 2000)
+        setTimeout(function(){
+          eyes.style = "display: none;"
+          homem.play()
+        }, 4000)
+      })
+
+
+
+      }
 
   setTimeout(function(){
     classes.replace("hide", "show");
   }, 1000)
+}
+
+function telefonema(a){
+  V = a
+  jumpscary.style = "display: flex;"
+  eyes.style = "display: none;"
+
+  if(V == false){
+    telefone.play();
+    setTimeout(function(){
+      mensagemDispensado.style = "display: flex;"
+      telaVitoria.style = "display: none;"
+
+    }, 2000)
+  } else if(V == true){
+    telaVitoria.style = "display: flex;"
+    mensagemDispensado.style = "display: none;"
+  }
+  onEnemy = false
 }
 
 function OfbehindWindow(){
